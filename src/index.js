a4713b51311501090525a0f433d1ebac9db240f2
Greta Ra <ggretute88@gmail.com>
2023-03-01 16:12:36 +0100

    Add example layout and styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css"
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?q=Arnhem&appid=55d93e41723e0921f44187affba5a537';
+async function getWeather (){
+  let response = await fetch(url, {mode:"cors"});
+  let weatherData = await response.json();
+  console.log(weatherData);
+  console.log(weatherData.main);
+  
+}
+
+getWeather();
+
