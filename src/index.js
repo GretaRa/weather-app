1fdf2895c6655ab1d72c4529042240917e12066a
Greta Ra <ggretute88@gmail.com>
2023-03-02 15:10:00 +0100

    Change function layout

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import "./styles.css";
 import { format } from "date-fns";
 
-async function getWeather() {
-	let url =
-		"https://api.openweathermap.org/data/2.5/weather?q=Arnhem&appid=55d93e41723e0921f44187affba5a537&units=metric";
-	let response = await fetch(url, { mode: "cors" });
-	let weatherData = await response.json();
-
-	const tempNow = document.getElementById("temperature");
-	tempNow.textContent = Math.round(weatherData.main.temp) + "°C";
-
-	const weatherNowDesc = document.getElementById("description");
-	weatherNowDesc.textContent = weatherData.weather[0].main;
-
-	const headerCity = document.getElementById("header-city");
-	headerCity.textContent = "Weather in" + " " + weatherData.name;
-
-	const weatherNowIcon = document.getElementById("weather-icon");
-	weatherNowIcon.src =
-		"http://openweathermap.org/img/wn/" +
-		weatherData.weather[0].icon +
-		"@2x.png";
-
-	const weatherDay = document.querySelector("#day");
-	weatherDay.textContent = format(new Date(weatherData.dt), "eeee");
-}
-
-getWeather();
-
 async function getForecast() {
 	let url =
 		"https://api.openweathermap.org/data/2.5/forecast?q=Arnhem&appid=55d93e41723e0921f44187affba5a537&units=metric";
@@ -37,8 +10,30 @@ async function getForecast() {
 
 	for (var i = 0; i < forecastData.list.length; i += 8) {
 		daily.push(forecastData.list[i]);
-	}
+	}	
+  
+  const headerCity = document.getElementById("header-city");
+	headerCity.textContent = "Weather in" + " " + daily[0].name;
 
+  //Create todays weather display
+  function displayTodayWeather() {
+    const weatherNowIcon = document.getElementById("weather-icon");
+    weatherNowIcon.src =
+      "http://openweathermap.org/img/wn/" +
+      daily[0].weather[0].icon +
+      "@2x.png";
+
+    const weatherDay = document.querySelector("#day");
+    weatherDay.textContent = format(new Date(daily[0].dt_txt), "eeee");
+
+    const tempNow = document.getElementById("temperature");
+    tempNow.textContent = Math.round(daily[0].main.temp_max) + "°C";
+
+    const weatherNowDesc = document.getElementById("description");
+    weatherNowDesc.textContent = daily[0].weather[0].main;
+  }
+  
+  //Create next 4 days weather display
 	function displayForecast() {
 		for (let d = 1; d < daily.length; d++) {
 			const forecastItem = document.createElement("div");
@@ -74,7 +69,7 @@ async function getForecast() {
 			forecastItem.appendChild(forecastLow);
 		}
 	}
-
+  displayTodayWeather()
 	displayForecast();
 }
 getForecast();
